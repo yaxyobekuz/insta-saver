9391dd50808672d8a41ea0b443fccedd47b7d027
yaxyobekuz <pubgn9642@gmail.com>
2026-01-11 22:42:00 +0500

    add: create Post model schema for MongoDB and remove Video model

---
+++ src/models/Post.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const Post = new mongoose.Schema(
+  {
+    title: { type: String, default: null },
+    source: { type: String, default: null },
+    view_count: { type: Number, default: 0 },
+    like_count: { type: Number, default: 0 },
+    thumbnail: { type: String, default: null },
+    downloadCount: { type: Number, default: 0 },
+    url: { type: String, required: true, unique: true },
+    medias: [
+      {
+        id: { type: String },
+        type: { type: String },
+        width: { type: Number },
+        height: { type: Number },
+        quality: { type: String },
+        mimeType: { type: String },
+        resolution: { type: String },
+        qualityLabel: { type: String },
+        url: { type: String, required: true },
+        fileId: { type: String, required: true },
+        extension: { type: String, default: null },
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Post", Post);
