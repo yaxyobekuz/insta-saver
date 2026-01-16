9d3b44a22a234e5fced7778db6bf2e0b774b237b
yaxyobekuz <pubgn9642@gmail.com>
2026-01-16 11:24:38 +0500

    feat: add agenda configuration for job scheduling and graceful shutdown

---
+++ src/config/agenda.config.js
@@ -0,0 +1,25 @@
+// Agenda
+const Agenda = require("agenda");
+
+// Create agenda instance
+const agenda = new Agenda({
+  maxConcurrency: 20,
+  processEvery: "10 seconds",
+  db: {
+    collection: "agendaJobs",
+    address: process.env.MONGO_URI,
+    options: { useNewUrlParser: true, useUnifiedTopology: true },
+  },
+});
+
+// Graceful shutdown
+const graceful = () => {
+  agenda.stop(() => {
+    process.exit(0);
+  });
+};
+
+process.on("SIGTERM", graceful);
+process.on("SIGINT", graceful);
+
+module.exports = agenda;
